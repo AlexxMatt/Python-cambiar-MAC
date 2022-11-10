7a1b39016bca6e14ad9fa8422fe00057f4a7284c
AlexxMatt <83834569+AlexxMatt@users.noreply.github.com>
2022-11-10 12:36:02 -0600

    Create Hola mundo.py

---
+++ Codigo_Basico/Hola mundo.py
@@ -0,0 +1,20 @@
+# Nuestro hola mundo en Python
+print("Hola Mundo, soy alexxmatt")
+
+# Esto es un comentario lo cual no afecta al codigo, puede servir de guia
+
+"""
+Este es un
+comentario
+pero se puede
+escribir en varias 
+lineas del codigo
+"""
+
+#Hay varios tipos de datos y de esta manera podemos consultar que tipo de dato son
+print(type("Este es un dato str")) # Tipo 'str'
+print(type(19)) # Tipo 'int'
+print(type(4.3)) # Tipo 'float'
+print(type(21 + 43j)) # Tipo 'complex'
+print(type(True)) # Tipo 'bool'
+print(type(print("Una cadena de texto"))) # Tipo 'NoneType'
