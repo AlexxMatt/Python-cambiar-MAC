dc360cfe7dc435394cd9b422637a19de467925e0
Alexis Emmanuel <83834569+AlexxMatt@users.noreply.github.com>
2022-11-14 13:34:33 -0600

    Create string.py

---
+++ Codigo_Basico/string.py
@@ -0,0 +1,21 @@
+Mstring = "Mi String"
+Ostring = 'Mi otro String'
+
+
+print(len(Mstring))
+print(len(Ostring))
+print(Mstring + " " + Ostring)
+
+Nstring = "Este es un String\ncon salto de línea"
+print(Nstring)
+
+Tstring = "\tEste es un String con tabulación"
+print(Tstring)
+
+Sstring = "\\tEste es un String \\n escapado"
+print(Sstring)
+
+lenguaje = "python"
+a, b, c, d, e, f = lenguaje
+print(a)
+print(e)
